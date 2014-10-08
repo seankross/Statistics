2482eeb5f8ddebbc0a6f49c969fb33d8c1d340fa
mertensu <mertens.ulf@gmail.com>
2014-10-08 09:52:07 +0200

    Change folders

---
+++ Termin_1/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+set.seed(123765)
+sex <- ifelse(runif(120)<.5,0,1)
+age <- floor(rnorm(120,22,3))
+
